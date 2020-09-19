e1f11e52a533073a652413369a96727e1dfe14bd
Luke Schwartz <luke@texasschwartz.com>
2020-09-19 16:47:17 -0400

    Added Cookies.js, updated readme

---
+++ Cookies.js
@@ -0,0 +1,82 @@
+var Cookie = function(key, value, options) {
+    this.config = {key, value, options}
+    this.pluses = /\+/g;
+    this.result = key ? undefined : {};
+
+    this.remove = function(key) {
+
+        if(this.Cookie(key) === undefined) return {
+            status: false,
+            message: "Cookie not found"
+        }
+        this.Cookie(key, {}, $.extend({}, options, { expires: -1 }));
+		return !this.Cookie(key);
+    }
+    this.cookie = (config) => {
+        if(config.key === undefined) {
+            this.result = this;
+        }
+        else if(config.value !== undefined) {
+            this.options = $.extend({}, config.defaults, options);
+            
+			if (typeof this.options.expires === 'number') {
+				var days = this.options.expires, t = this.options.expires = new Date();
+				t.setTime(+t + days * 864e+5);
+            }
+
+			return (document.cookie = [
+				this.encode(key), '=', this.stringifyCookieValue(value),
+				this.options.expires ? '; expires=' + this.options.expires.toUTCString() : '',
+				this.options.path    ? '; path='    + this.options.path : '',
+				this.options.domain  ? '; domain='  + this.options.domain : '',
+				this.options.secure  ? '; secure' : ''
+            ].join(''));
+            
+        }
+        else {
+            var cookies = document.cookie ? document.cookie.split('; ') : [];
+            
+            for(var i = 0; i < cookies.length; i++) {
+                var parts = cookies[i].split('=');
+                var name = this.decode(parts.shift());
+                var cookie = parts.join('=');
+                
+                if (config.key && config.key === name) {
+                    this.result = this.read(cookie, value);
+                    break;
+                }
+    
+                if (!key && (cookie = read(cookie)) !== undefined) {
+                    this.result[name] = cookie;
+                }
+            }
+        }
+    }
+    this.encode = (s) => {
+        return encodeURIComponent(s);
+    }
+    this.decode = (s) => {
+        return decodeURIComponent(s);
+    }
+    this.read = (s, converter) => {
+        var value = parseCookieValue(s);
+		return $.isFunction(converter) ? converter(value) : value;
+    }
+    this.parseCookieValue = (s) => {
+        if (s.indexOf('"') === 0) {
+			
+			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+		}
+        
+		try {
+            
+            s = decodeURIComponent(s.replace(pluses, ' '));
+			return config.json ? JSON.parse(s) : s;
+		} catch(e) {}
+    }
+    this.stringifyCookieValue = (value) => {
+		return this.encode(JSON.stringify(value));
+	}
+    this.cookie(this.config);
+    return this.result;
+} 
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# cookies
+# Cookies.js Overview
+
+Cookies.js is used to work around a unique jquery.cookies error that occurs. Some, including myself, encountered errors when using `$.cookie`. I decided to take it upon myself to create an easy to use cookie method for anyone.
+
+# Documentation
+
+Cookie is a function that accepts up to three arguments, and has a remove function.
+The 3 arguments are `key, value, options`. 
+Keys:
+> Keys must be a string, and is used to fetch or set cookies.
+
+Value: 
+> Value must be an object, and is used when setting cookies.
+
+Options:
+> Options must be an object, and has several settings:
+> - `expires`: Sets the expiration date for the cookie (in days).
+> - `path`: Sets the path for the cookie.
+> - `domain`: Sets the domain for the cookie
+> - `secure`: Cookie can only be transmitted over https.
+> No options are required however:
+> - Expires is reccomended
+> - Other options are for security and request handling, so not required.
+
+
+# Examples
+
+```js
+//Cookies function:
+
+/* No arguments: returns document
+ * Supply only key: fetches cookie at given key
+ * All arguments: sets cookie at given key
+ */
+Cookie(key, value, options);
+
+/* Example: Setting a cookie that expires in one day
+ * Key: Must be of type string
+ * Value: Must be of type object
+ * Options: Must be of type object
+ */
+Cookie('myCookie', {data: "Hello World!"}, {expires: 1});
+
+/* Example: Fetching a cookie with key "myCookie"
+ * Key: Must be of type string
+ */
+Cookie('myCookie'); // Given previous example, returns {data: "Hello World"}
+
+/* Example: Removing a cookie given key "myCookie"
+ * Key: Must be supplied in the remove function
+ */
+Cookie().remove('myCookie');
+
+
+//It is good practice to check if the cookie exists before fetching it, i.e.
+if(!!Cookie('myCookie')) {
+    console.log(Cookie('myCookie'));
+}
+
+
+```
